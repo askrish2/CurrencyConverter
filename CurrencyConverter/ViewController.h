ca4f53a8db3e2027768831377cf87de1ef3bd292
Ash Krishnan <askrish2@illinois.edu>
2016-09-06 17:38:07 -0500

    Initial Commit

---
+++ CurrencyConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyConverter
+//
+//  Created by Ashwini Krishnan on 9/6/16.
+//  Copyright © 2016 Ash Krishnan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CurrencyConverter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CurrencyConverter
+//
+//  Created by Ashwini Krishnan on 9/6/16.
+//  Copyright © 2016 Ash Krishnan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
